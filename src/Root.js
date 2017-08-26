f0b26eee9ef6c927ad51dcf6daa82a3938bce787
dominik-th <dominik-th@users.noreply.github.com>
2017-08-26 23:35:13 +0200

    Add piwik analytics

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter  as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
-
+import createHistory from 'history/createBrowserHistory'
+import PiwikReactRouter from 'piwik-react-router';
 import App from './App';
+import config from './env/config';
+
+var history = createHistory();
+
+if (config.piwik) {
+  history = PiwikReactRouter(config.piwik).connectToHistory(history);
+}
 
 const Root = ({ store }) => (
-  <Router>
+  <Router history={history}>
     <Switch>
       <Route path="/:imageId" component={App} />
       <Route path="/" component={App} />
