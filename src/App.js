507e355e9085198091dacbdc2da0818a91b3de74
dominik-th <dominik-th@users.noreply.github.com>
2017-09-01 00:58:16 +0200

    Move route parameter handling to component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { loadCoverImage } from './actions'
+import _ from 'lodash';
+import storage from './env/storage.json';
 import Cover from './Cover';
 import ContentGrid from './ContentGrid';
 import './App.css';
@@ -9,19 +10,8 @@ import './App.css';
 // exiftool -j -n
 // ImageMagick:
 // convert -strip -interlace Plane -gaussian-blur 0.05 -quality 75% -resize 3840x2160^ source.jpg destination.jpg
-// TODO: switch to Redux!
 
 class App extends Component {
-
-  componentWillMount() {
-    this.props.showCoverImage(this.props.currentImageId);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.currentImageId !== nextProps.currentImageId) {
-      this.props.showCoverImage(nextProps.currentImageId);
-    }
-  }
 
   render() {
     return (
@@ -39,24 +29,21 @@ class App extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    showCoverImage: (id) => {
-      dispatch(loadCoverImage(id));
-    }
-  };
-};
-
 const mapStateToProps = (state, props) => {
+  const randomIndex = _.random(0, storage.images.length - 1);
+  const paramIndex = _.findIndex(storage.images, ['id', props.match.params.imageId]);
+  const currentIndex = paramIndex === -1 ? randomIndex : paramIndex;
+
+  const prevIndex = (currentIndex - 1) < 0 ? storage.images.length - 1 : currentIndex -1;
+  const nextIndex = (currentIndex + 1) % storage.images.length;
+
   return {
-    currentImageId: props.match.params.imageId,
-    currentImage: state.cover.currentImage,
-    prevImage: state.cover.prevImage,
-    nextImage: state.cover.nextImage
+    currentImage: storage.images[currentIndex],
+    prevImage: storage.images[prevIndex],
+    nextImage: storage.images[nextIndex]
   };
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  mapStateToProps
 )(App);
