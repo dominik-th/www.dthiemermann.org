9a4215c6b7b32342e95826fe56d40d9aa133d5ed
dominik-th <dominik-th@users.noreply.github.com>
2018-01-06 17:33:43 +0100

    Add sample configuration

---
+++ src/env/config.sample.js
@@ -0,0 +1,11 @@
+module.exports = {
+  piwik: {
+    url: 'https://piwik.example.com',
+    siteId: 1,
+    clientTrackerName: 'piwik.js',
+    serverTrackerName: 'piwik.php'
+  }
+  backend: {
+    url: 'https://app.example.com/api/',
+  }
+}
