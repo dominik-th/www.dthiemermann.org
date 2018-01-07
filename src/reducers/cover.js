1a69d5217c484d1a5358d54556d4cfe47fc0981d
dominik-th <dominik-th@users.noreply.github.com>
2018-01-07 18:12:47 +0100

    Add cover reducer

--- src/reducers/cover.js
+++ src/reducers/cover.js
@@ -1,8 +1,23 @@
+import { REQUEST_PHOTOS, RECEIVE_PHOTOS } from '../actions';
+
 const initialState = {
+  loading: true,
+  storage: null
 };
 
 export default function cover(state = initialState, action) {
   switch (action.type) {
+
+    case REQUEST_PHOTOS:
+      return Object.assign({}, state, {
+        loading: true,
+      });
+
+    case RECEIVE_PHOTOS:
+      return Object.assign({}, state, {
+        loading: false,
+        storage: action.photos
+      });
 
     default:
       return state;
