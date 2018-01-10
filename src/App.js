707c98903d7c86edb3d0d084900cd26544878034
dominik-th <dominik-th@users.noreply.github.com>
2018-01-10 18:26:41 +0100

    Replace local storage.json with dynamic remote api

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import storage from './env/storage.json';
+import { fetchPhotos } from './actions';
+import config from './env/config';
+import LoadingScreen from './LoadingScreen';
 import Cover from './Cover';
 import ContentGrid from './ContentGrid';
-import './App.css';
-
-// Exiftool:
-// exiftool -j -n
-// ImageMagick:
-// convert -strip -interlace Plane -gaussian-blur 0.05 -quality 75% -resize 3840x2160^ source.jpg destination.jpg
 
 class App extends Component {
+
+  componentWillMount() {
+    this.props.dispatch(fetchPhotos());
+  }
 
   render() {
     return (
       <div className="App">
-        <Cover
-          imageUrl={`images/${this.props.currentImage.fileName}`}
-          prevImage={this.props.prevImage}
-          nextImage={this.props.nextImage}
-        />
-        <ContentGrid
-          image={this.props.currentImage}
-        />
+        { this.props.loading ? (
+            <div>
+              <LoadingScreen subheader={config.website.title} />
+            </div>
+          ) : (
+            <div>
+              <Cover
+                imageUrl={`${config.backend.url}/photo/${this.props.currentImage.id}`}
+                prevImage={this.props.prevImage}
+                nextImage={this.props.nextImage}
+              />
+              <ContentGrid
+                image={this.props.currentImage}
+              />
+            </div>
+          ) }
       </div>
     );
   }
 }
 
-const mapStateToProps = (state, props) => {
-  const randomIndex = _.random(0, storage.images.length - 1);
-  const paramIndex = _.findIndex(storage.images, ['id', props.match.params.imageId]);
-  const currentIndex = paramIndex === -1 ? randomIndex : paramIndex;
-
-  const prevIndex = (currentIndex - 1) < 0 ? storage.images.length - 1 : currentIndex -1;
-  const nextIndex = (currentIndex + 1) % storage.images.length;
-
-  return {
-    currentImage: storage.images[currentIndex],
-    prevImage: storage.images[prevIndex],
-    nextImage: storage.images[nextIndex]
+const mapStateToProps = (state, ownProps) => {
+  const { storage, loading } = state.cover;
+  const stateProps = {
+    loading
   };
+  if (storage) {
+    const randomIndex = _.random(0, storage.images.length - 1);
+    const paramIndex = _.findIndex(storage.images, ['id', ownProps.match.params.imageId]);
+    const currentIndex = paramIndex === -1 ? randomIndex : paramIndex;
+
+    const prevIndex = (currentIndex - 1) < 0 ? storage.images.length - 1 : currentIndex -1;
+    const nextIndex = (currentIndex + 1) % storage.images.length;
+
+    stateProps.currentImage = storage.images[currentIndex];
+    stateProps.prevImage = storage.images[prevIndex];
+    stateProps.nextImage = storage.images[nextIndex];
+  }
+
+  return stateProps;
 };
 
 export default connect(
