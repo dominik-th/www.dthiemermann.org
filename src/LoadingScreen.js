dbc6466297d55b8fc81ae6e4a401e907a30bbafd
dominik-th <dominik-th@users.noreply.github.com>
2018-01-10 18:36:16 +0100

    Fix LoadingScreen position

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Dimmer, Header, Loader } from 'semantic-ui-react';
+import './LoadingScreen.css';
 
 class LoadingScreen extends Component {
   render() {
     return (
-      <Dimmer active>
-        <Loader size="medium" >
-          <Header as="h3" inverted color="teal" content="Loading..." subheader={this.props.subheader} />
-        </Loader>
-      </Dimmer>
+      <div className="LoadingScreen">
+        <Dimmer active>
+          <Loader size="medium" >
+            <Header as="h3" inverted color="teal" content="Loading..." subheader={this.props.subheader} />
+          </Loader>
+        </Dimmer>
+      </div>
     )
   }
 }
