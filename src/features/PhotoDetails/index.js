150bdfa0b2738feab6410e00f7507b88109ccea2
dominik-th <dominik-th@users.noreply.github.com>
2018-02-11 18:08:38 +0100

    Use CircleMarker for map

--- src/features/PhotoDetails/index.js
+++ src/features/PhotoDetails/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Map, Marker, TileLayer } from 'react-leaflet';
+import { Map, CircleMarker, TileLayer } from 'react-leaflet';
 import Title from './Title';
 import Specs from './Specs';
 import './PhotoDetails.css';
@@ -19,9 +19,11 @@ class PhotoDetails extends Component {
     const center = [size.x/2, size.y/2];
     const distance = mouseEvent.containerPoint.distanceTo(center)/size.distanceTo(center);
     if (distance < 0.15) {
-      this.refs.map.leafletElement.setView(this.refs.location.props.position, 15, {animate: false});
+      this.refs.map.leafletElement.setView(this.refs.location.props.center, 15, {animate: false});
+      this.refs.location.leafletElement.setRadius(30);
     } else {
       this.refs.map.leafletElement.setZoom(11);
+      this.refs.location.leafletElement.setRadius(15);
     }
   }
 
@@ -39,8 +41,12 @@ class PhotoDetails extends Component {
                 zoom={6}
                 center={location}
                 zoomAnimation={false}
-                dragging={false}
+                zoomControl={false}
                 scrollWheelZoom={false}
+                doubleClickZoom={false}
+                boxZoom={false}
+                keyboard={false}
+                dragging={false}
                 onMouseover={this.onMapEnter}
                 onMouseout={this.onMapExit}
                 onMousemove={this.onMapHover}
@@ -49,7 +55,7 @@ class PhotoDetails extends Component {
                   url='https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZG9taW5pay10aCIsImEiOiJjajZxYnF4ZnowN25qMzJvNnAyMzF0OW13In0.wt8ntpDLnptHNro4B7S7Fg'
                   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={location} ref="location" />
+                <CircleMarker ref="location" center={location} radius={15} color="" />
               </Map>
             </div>
             <div className="Flex-Item">
