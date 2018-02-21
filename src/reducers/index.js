7318d88914cb8d75926548d6267023247ee4651f
dominik-th <dominik-th@users.noreply.github.com>
2018-02-21 14:54:25 +0100

    Fix missing reducer for react-router-redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 import auth from './auth';
 import cover from './cover';
 
 const rootReducer = combineReducers({
   auth,
-  cover
+  cover,
+  routing: routerReducer,
 });
 
 export default rootReducer;
