77d79cfc303aab3ef858e85279e50b2233ba9292
dominik-th <dominik-th@users.noreply.github.com>
2018-03-03 17:59:27 +0100

    Add map marker clustering

--- src/features/PhotoMap/index.js
+++ src/features/PhotoMap/index.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Map, TileLayer,ZoomControl } from 'react-leaflet';
+import { Map, TileLayer, ZoomControl, Marker } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import 'react-leaflet-markercluster/dist/styles.min.css';
 import './PhotoMap.css';
 
 class PhotoMap extends Component {
   render() {
+    const markers = [];
     return (
       <div className="Photo-Map-Container">
         <Map
           center={[0, 0]}
           zoom={3}
           zoomControl={false}
+          maxZoom={18}
         >
-          <ZoomControl position="bottomleft" />
           <TileLayer
             url='https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZG9taW5pay10aCIsImEiOiJjajZxYnF4ZnowN25qMzJvNnAyMzF0OW13In0.wt8ntpDLnptHNro4B7S7Fg'
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
+          <ZoomControl position="bottomleft" />
+          <MarkerClusterGroup markers={markers} options={{showCoverageOnHover: false}} />
         </Map>
       </div>
     );
