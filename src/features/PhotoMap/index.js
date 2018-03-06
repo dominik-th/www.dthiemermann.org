7a1515b1556cec1527d7bbaef982708645a2dbe8
dominik-th <dominik-th@users.noreply.github.com>
2018-03-06 11:52:19 +0100

    Add basic functionality to the PhotoMap component

--- src/features/PhotoMap/index.js
+++ src/features/PhotoMap/index.js
@@ -1,31 +1,72 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Map, TileLayer, ZoomControl, Marker } from 'react-leaflet';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer, ZoomControl, Marker, Popup } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
+import RouterForwarder from '../../utils/RouterForwarder';
 import 'react-leaflet-markercluster/dist/styles.min.css';
 import './PhotoMap.css';
 
+const propTypes = {
+  mapboxToken: PropTypes.string,
+  markers: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string,
+      location: PropTypes.arrayOf(PropTypes.number),
+      title: PropTypes.string,
+      url: PropTypes.string,
+    })
+  ),
+}
+
+const contextTypes = {
+  router: PropTypes.object,
+}
+
+const defaultProps = {
+  markers: [],
+}
+
 class PhotoMap extends Component {
   render() {
-    const markers = [];
+    const markers = this.props.markers.map(marker => {
+      return (
+        <Marker key={marker.key} position={marker.location}>
+          <Popup>
+            <RouterForwarder context={this.context}>
+              {marker.title}
+              <br />
+              <Link to={marker.url}>Details</Link>
+            </RouterForwarder>
+          </Popup>
+        </Marker>
+      )
+    });
     return (
       <div className="Photo-Map-Container">
         <Map
           center={[0, 0]}
           zoom={3}
           zoomControl={false}
+          minZoom={3}
           maxZoom={18}
         >
           <TileLayer
-            url='https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZG9taW5pay10aCIsImEiOiJjajZxYnF4ZnowN25qMzJvNnAyMzF0OW13In0.wt8ntpDLnptHNro4B7S7Fg'
+            url={`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=${this.props.mapboxToken}`}
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
           <ZoomControl position="bottomleft" />
-          <MarkerClusterGroup markers={markers} options={{showCoverageOnHover: false}} />
+          <MarkerClusterGroup options={{showCoverageOnHover: false}}>
+            { markers }
+          </MarkerClusterGroup>
         </Map>
       </div>
     );
   }
 }
+
+PhotoMap.propTypes = propTypes;
+PhotoMap.contextTypes = contextTypes;
+PhotoMap.defaultProps = defaultProps;
 
 export default PhotoMap;
