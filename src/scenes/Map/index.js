fc5a61bc881444899192b8f696ab11b63cf43349
dominik-th <dominik-th@users.noreply.github.com>
2018-03-06 11:24:41 +0100

    Pass required photos information to map component

--- src/scenes/Map/index.js
+++ src/scenes/Map/index.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPhotos } from '../../actions';
 import config from '../../env/config';
 import PhotoMap from '../../features/PhotoMap';
 
 class Map extends Component {
+  componentWillMount() {
+    if (!this.props.markers.length) {
+      this.props.dispatch(fetchPhotos());
+    }
+  }
+
   render() {
     return (
-      <PhotoMap />
+      <PhotoMap mapboxToken={config.mapbox.apiKey} markers={this.props.markers} />
     );
   }
 }
 
-export default Map;
+const mapStateToProps = (state, ownProps) => {
+  const { storage } = state.cover;
+  const stateProps = {
+    markers: [],
+  };
+  if (storage) {
+    stateProps.markers = storage.images.map(image => {
+      return {
+        key: image.id,
+        location: [image.location.lat, image.location.long],
+        title: image.title,
+        url: `/photo/${image.id}`,
+      }
+    });
+  }
+  return stateProps;
+};
+
+export default connect(
+  mapStateToProps
+)(Map);
